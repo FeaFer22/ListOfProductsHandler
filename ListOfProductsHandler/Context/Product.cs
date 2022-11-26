d8375a329c6b7ffc1ec3ec6dae881dbff576ac40
Soleev Maksim <msoleev98@gmail.com>
2022-11-26 16:35:02 +0300

    Установка пакетов Entity Framework, создание модели Product

---
+++ ListOfProductsHandler/Context/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ListOfProductsHandler.Context
+{
+    public class Product
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Название")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Описание")]
+        public string Description { get; set; }
+        [Required]
+        [Display(Name = "Цена")]
+        public ushort Price { get; set; }
+    }
+}
